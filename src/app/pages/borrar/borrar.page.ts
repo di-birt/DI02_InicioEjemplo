ae295ac9db1adb6ed675ece2bc5d75f308e2d5c6
di-birt <mardanza@birt.eus>
2024-09-19 12:24:51 +0200

    primer commit

---
+++ src/app/pages/borrar/borrar.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-borrar',
+  templateUrl: './borrar.page.html',
+  styleUrls: ['./borrar.page.scss'],
+})
+export class BorrarPage implements OnInit {
+  indice: string = "";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick() {
+    // Borrar
+  }
+}
